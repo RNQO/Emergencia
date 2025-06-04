90c48e84dd9267dd19e95b197bc7362c74a8a224
RNQO <153382479+RNQO@users.noreply.github.com>
2025-06-04 13:28:41 -0400

    script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+
 const form = document.getElementById('formularioPaciente');
 const tabla = document.getElementById('tablaPacientes').querySelector('tbody');
 const authSection = document.getElementById('authSection');
@@ -101,31 +102,7 @@ function validarFormulario(datos) {
 
 function agregarPaciente(paciente) {
 }
-function validarFormulario(datos) {
-  if (
-    !datos.nombre || !datos.edad || !datos.genero || !datos.documento ||
-    !datos.sintomas || !datos.gravedad || !datos.tratamiento ||
-    !datos.medicamentos || !datos.examenes
-  ) {
-    mostrarMensaje("Por favor, complete todos los campos.", false);
-    return false;
-  }
 
-  if (isNaN(datos.edad) || datos.edad <= 0) {
-    mostrarMensaje("Edad debe ser un número mayor a 0.", false);
-    return false;
-  }
-
-  if (datos.documento.length < 5) {
-    mostrarMensaje("Documento debe tener al menos 5 caracteres.", false);
-    return false;
-  }
-
-  if (datos.gravedad === "critico") {
-    mostrarMensaje("⚠️ Paciente en estado crítico registrado.", true);
-  } else {
-    mostrarMensaje("", true);
-  }
 
   return true;
 }
